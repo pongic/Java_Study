fa543e2604a9230c3340a490ccf44747529c591d
pongic <pongic@naver.com>
2022-09-05 20:33:56 +0900

    2022.09.05 update

---
+++ BOJ/단계별로풀어보기/입출력과사칙연산/No_10172.java
@@ -0,0 +1,11 @@
+package BOJ.단계별로풀어보기.입출력과사칙연산;
+
+public class No_10172 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
