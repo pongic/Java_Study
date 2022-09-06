2df54158cd32264c0550a2174ca62f7e8e2350e8
pongic <pongic@naver.com>
2022-09-06 19:28:13 +0900

    2022.09.06 update

---
+++ BOJ/단계별로풀어보기/조건문/No_2480.java
@@ -0,0 +1,33 @@
+package BOJ.단계별로풀어보기.조건문;
+import java.util.Map;
+import java.util.Scanner;
+
+public class No_2480 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int max = 0;
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int num3 = input.nextInt();
+        int result = 0;
+        if (num1 == num2 && num2 == num3) {
+            result = 10000 + (num1 * 1000);
+        }
+        else if (num1 == num2) {
+            result = 1000 + (num1 * 100);
+        }
+        else if (num2 == num3) {
+            result = 1000 + (num2 * 100);
+        }
+        else if (num1 == num3) {
+            result = 1000 + (num1 * 100);
+        }
+        else {
+            max = Math.max(num1,num2);
+            max = Math.max(max,num3);
+            result = max * 100;
+        }
+        System.out.println(result);
+    }
+}
