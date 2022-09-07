d2d3d90f9c330e7a0773cf3fd4af7997057fbea4
pongic <pongic@naver.com>
2022-09-07 22:08:00 +0900

    2022.09.07 update

---
+++ BOJ/단계별로풀어보기/반복문/No_15552.java
@@ -0,0 +1,11 @@
+package BOJ.단계별로풀어보기.반복문;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class No_15552 {
+    public static void main(String[] args) {
+
+    }
+}
