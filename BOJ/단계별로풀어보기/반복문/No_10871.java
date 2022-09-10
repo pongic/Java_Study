2ab436c666880f6b06c5392213472c636232943f
pongic <pongic@naver.com>
2022-09-10 17:10:07 +0900

    2022.09.10 update

--- BOJ/단계별로풀어보기/반복문/No_10871.java
+++ BOJ/단계별로풀어보기/반복문/No_10871.java
@@ -5,8 +5,17 @@ public class No_10871 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        int N = input.nextInt();
+        int X = input.nextInt();
+        int[] A = new int[N];
 
-
-
+        for (int i = 0; i < N; i++) {
+            A[i] = input.nextInt();
+        }
+        for (int j = 0; j < N; j++) {
+            if (A[j] < X) {
+                System.out.print(A[j] + " ");
+            }
+        }
     }
 }
