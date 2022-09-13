4b550a118992ec5e87ee4312063cd416c91920c8
pongic <pongic@naver.com>
2022-09-13 22:00:46 +0900

    2022.09.13 update

--- BOJ/단계별로풀어보기/배열/No_10818.java
+++ BOJ/단계별로풀어보기/배열/No_10818.java
@@ -1,4 +1,5 @@
 package BOJ.단계별로풀어보기.배열;
+import java.util.Arrays;
 import java.util.Scanner;
 public class No_10818 {
     public static void main(String[] args) {
@@ -6,21 +7,21 @@ public class No_10818 {
 
         int N = input.nextInt();
         int[] A = new int[N];
-        int max = 0;
-        int min;
 
         for (int i = 0; i < N; i++) {
             A[i] = input.nextInt();
         }
-        for (int i = 0; i < N; i++) {
-            if (A[i] >= max)
-                max = A[i];
-        }
-        min = max;
-        for (int i = 0; i < N; i++) {
-            if (A[i] <= min)
-                min = A[i];
-        }
-        System.out.println(min + " " + max);
+        Arrays.sort(A);
+        System.out.println(A[0] + " " + A[N - 1]);
+//        for (int i = 0; i < N; i++) {
+//            if (A[i] >= max)
+//                max = A[i];
+//        }
+//        min = max;
+//        for (int i = 0; i < N; i++) {
+//            if (A[i] <= min)
+//                min = A[i];
+//        }
+//      System.out.println(min + " " + max);
     }
 }
