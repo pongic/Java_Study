caf50d68d49f11c417cad013ee7271c368ecf2bd
pongic <pongic@naver.com>
2022-09-14 22:39:28 +0900

    2022.09.14 update

--- BOJ/단계별로풀어보기/배열/No_3052.java
+++ BOJ/단계별로풀어보기/배열/No_3052.java
@@ -1,19 +1,23 @@
 package BOJ.단계별로풀어보기.배열;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+
 public class No_3052 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         int[] arr = new int[10];
-        int result = 0;
+
         for (int i = 0; i < 10; i++) {
             arr[i] = input.nextInt();
+            arr[i] = arr[i] % 42;
         }
-        for (int i = 0; i < 9; i++) {
-            if (arr[i] % 42 == 0) {
 
-            }
+        for (int arrs : arr) {
         }
+
 
     }
 }
