9b0e9990d2e2ac45b1b0c9869f3c87a2a8ce427c
pongic <pongic@naver.com>
2022-09-14 22:28:12 +0900

    2022.09.14 update

--- BOJ/단계별로풀어보기/배열/No_2562.java
+++ BOJ/단계별로풀어보기/배열/No_2562.java
@@ -12,16 +12,9 @@ public class No_2562 {
         for (int i = 0; i < 9; i++) {
             arr[i] = input.nextInt();
         }
-        for (int i = 0; i < 9; i++) {
-            if (arr[i] >= max) {
-                max = arr[i];
-            }
-        }
-        for (int j = 0; j < 9; j++) {
-            if (max != arr[j]) {
-                index++;
-            }
-        }
+        Arrays.sort(arr);
+        max = arr[8];
+
 
         System.out.println(max);
         System.out.println(index);
---
+++ BOJ/단계별로풀어보기/배열/No_3052.java
@@ -0,0 +1,19 @@
+package BOJ.단계별로풀어보기.배열;
+import java.util.Scanner;
+public class No_3052 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        int result = 0;
+        for (int i = 0; i < 10; i++) {
+            arr[i] = input.nextInt();
+        }
+        for (int i = 0; i < 9; i++) {
+            if (arr[i] % 42 == 0) {
+
+            }
+        }
+
+    }
+}
