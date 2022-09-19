7a58cc5d9fdf33808196a37e907b3153404057bb
pongic <pongic@naver.com>
2022-09-19 21:34:31 +0900

    2022.09.19 update

--- BOJ/단계별로풀어보기/반복문/No_1110.java
+++ BOJ/단계별로풀어보기/반복문/No_1110.java
@@ -2,32 +2,24 @@ package BOJ.단계별로풀어보기.반복문;
 import java.util.Scanner;
 public class No_1110 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
 
-        int N = input.nextInt();
-        int NQuo, NRemain, result;
-        int count = 1;
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        in.close();
+
+        int cnt = 0;
+        int copy = N;
 
         while (true) {
-            if (N < 10) {
-                NQuo = 0;
-                NRemain = N;
-            } else {
-                NQuo = N / 10;
-                NRemain = N % (NQuo * 10);
+            N = ((N % 10) * 10) + (((N / 10) + (N % 10)) % 10);
+            cnt++;
+
+            if (copy == N) {
+                break;
             }
-            result = NQuo + NRemain;
         }
-//        while (true) {
-//            if (N == (NRemain * 10) + AndN) {
-//                System.out.println(count);
-//                break;
-//            }
-//            else {
-//                N = NRemain * 10 + AndN;
-//                count++;
-//            }
-//        }
+        System.out.println(cnt);
 
     }
 }
