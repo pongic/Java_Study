dc957f9cc575111623fcef5a0b90495f1659be4c
pongic <pongic@naver.com>
2022-09-20 20:35:56 +0900

    2022.09.20 update

--- BOJ/단계별로풀어보기/배열/No_2562.java
+++ BOJ/단계별로풀어보기/배열/No_2562.java
@@ -7,17 +7,23 @@ public class No_2562 {
 
         int[] arr = new int[9];
         int max = 0;
+        int count = 0;
         int index = 0;
 
         for (int i = 0; i < 9; i++) {
             arr[i] = input.nextInt();
         }
-        Arrays.sort(arr);
-        max = arr[8];
+        input.close();
 
+        for (int i : arr) {
+            count++;
 
+            if (i > max) {
+                max = i;
+                index = count;
+            }
+        }
         System.out.println(max);
         System.out.println(index);
-
     }
 }
