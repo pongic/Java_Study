ef1819d72ca2910a07f7e12198813732305d19e7
pongic <pongic@naver.com>
2022-09-22 20:46:48 +0900

    2022.09.22 update

---
+++ BOJ/단계별로풀어보기/배열/No_8958.java
@@ -0,0 +1,14 @@
+package BOJ.단계별로풀어보기.배열;
+import java.util.Scanner;
+
+public class No_8958 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int T = input.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            ;
+        }
+    }
+}
