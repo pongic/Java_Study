5fa767b7dfeb7e8d8f1b3020214d5e1cc976e196
pongic <pongic@naver.com>
2022-09-23 22:08:53 +0900

    2022.09.23 update

--- BOJ/단계별로풀어보기/배열/No_8958.java
+++ BOJ/단계별로풀어보기/배열/No_8958.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 public class No_8958 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
-        int T = input.nextInt();
-
-        for (int i = 0; i < T; i++) {
-            ;
+        // 테스트 케이스 입력 받은 후 배열 선언
+        String[] arr = new String[input.nextInt()];
+        // String 배열에 값 넣기
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = input.nextLine();
         }
+
     }
 }
