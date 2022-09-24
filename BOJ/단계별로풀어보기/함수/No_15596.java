bee35ed455327b1480dd05a905d512056549720e
pongic <pongic@naver.com>
2022-09-24 20:31:44 +0900

    2022.09.24 update

---
+++ BOJ/단계별로풀어보기/함수/No_15596.java
@@ -0,0 +1,11 @@
+package BOJ.단계별로풀어보기.함수;
+
+public class No_15596 {
+    public long sum (int[] a) {
+        long sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        return sum;
+    }
+}
