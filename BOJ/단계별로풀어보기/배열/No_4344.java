3c22c1d6d6b50172206e7e4d29f30e7c5bedf1cd
pongic <pongic@naver.com>
2022-09-24 20:24:22 +0900

    2022.09.24 update

--- BOJ/단계별로풀어보기/배열/No_4344.java
+++ BOJ/단계별로풀어보기/배열/No_4344.java
@@ -4,25 +4,26 @@ public class No_4344 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        // 테스트 케이스 입력 받기
-        int testCase = in.nextInt();
+        int testCase = in.nextInt();  // 테스트 케이스 입력
         for (int i = 0; i < testCase; i++) {
-            int N = in.nextInt(); // 학생 수 입력 받기
-            int[] arr = new int[N];
-            double sum = 0; // 성적 합
-            for (int j = 0; j < arr.length; j++) {
-                arr[j] = in.nextInt(); // 학생 성적 입력
-                sum += arr[j];
-            }
-            double avg = sum / arr.length;
-            double count = 0; // 평균 넘는 학생 수
+            int N = in.nextInt();  // 학생 수 입력
+            int[] scores = new int[N]; // 학생 점수 입력 할 배열 생성
 
-            // 평균 넘는 학생 수 조회
-            for (int k = 0; k < arr.length; k++) {
-                if (arr[k] > avg) count++;  // 평균이 넘었을 때 count 증가
+            double sum = 0;
+            double avg = 0; // 평균
+            double count = 0; // 평균 넘는 학생 있을 때 증가
+
+            for (int j = 0; j < N; j++) {
+                scores[j] = in.nextInt();  // 학생 점수 입력
+                sum += scores[j];  // 학생 점수 합
+            }
+
+            avg = (double) sum / N;
+
+            for (int k = 0; k < N; k++) {  // 학생 점수랑 평균 비교
+                if (scores[k] > avg) count++;
             }
             System.out.printf("%.3f%%\n",(count / N) * 100);
         }
-        in.close();
     }
 }
