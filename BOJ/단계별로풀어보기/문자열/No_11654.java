7d01ed359022245933d51b6b36df35f5c423318c
pongic <pongic@naver.com>
2022-09-28 20:56:33 +0900

    2022.09.28 update

---
+++ BOJ/단계별로풀어보기/문자열/No_11654.java
@@ -0,0 +1,12 @@
+package BOJ.단계별로풀어보기.문자열;
+import java.util.Scanner;
+public class No_11654 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    char res = in.nextLine().charAt(0);
+    int result = (int)res;
+
+    System.out.println(result);
+  }
+}
