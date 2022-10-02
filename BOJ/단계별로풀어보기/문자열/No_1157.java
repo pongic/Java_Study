7cfeb4e5ec704266f717116d826c710d4f1e9121
pongic <pongic@naver.com>
2022-10-02 19:51:06 +0900

    2022.10.02 update

--- BOJ/단계별로풀어보기/문자열/No_1157.java
+++ BOJ/단계별로풀어보기/문자열/No_1157.java
@@ -1,52 +1,30 @@
 package BOJ.단계별로풀어보기.문자열;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class No_1157 {
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    List<Character> list = new ArrayList<>();
-    List<Character> newList = new ArrayList<>();
-    int idx = 0;
+    String S = in.next().toUpperCase();
+
+    int[] alphabet = new int[26];
+    int num = 0;
     int max = 0;
-    int maxIdx = 0;
+    char result = '?';
 
-    String S = in.nextLine();
-    String subS = S.toUpperCase();
-
-    for (int i = 0; i < subS.length(); i++) {
-      list.add(subS.charAt(i));
+    for (int i = 0; i < S.length(); i++) {
+      num = S.charAt(i) - 'A';
+      alphabet[num]++;
     }
-    newList = list.stream().distinct().collect(Collectors.toList());
-    int[] count = new int[newList.size()];
 
-   for (int i = 0; i < newList.size(); i++) {
-     for (int j = 0; j < subS.length(); j++) {
-       if (newList.get(i).equals(subS.charAt(j))) {
-         idx++;
-       }
-     }
-     count[i] = idx;
-     idx = 0;
-   }
-   max = count[0];
-   for (int i = 0; i < count.length; i++) {
-     if (count[i] > max) {
-       max = count[i];
-       maxIdx++;
-     }
-   }
-   for (int i = 0; i < count.length; i++) {
-     if (max == count[i]) {
-       System.out.println("?");
-       break;
-     }
-     else {
-       System.out.println(newList.get(maxIdx));
-       break;
-     }
-   }
+    for (int i = 0; i < alphabet.length; i++) {
+      if (alphabet[i] > max) {
+        max = alphabet[i];
+        result = (char)(i+'A');
+      }
+      else if (max == alphabet[i]) {
+        result = '?';
+      }
+    }
+    System.out.println(result);
   }
 }
