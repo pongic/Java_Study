365f16f57ef4947b8b6b3a2d5437ebcd60c1b1bb
pongic <pongic@naver.com>
2022-10-06 23:22:22 +0900

    2022.10.06 update

---
+++ BOJ/단계별로풀어보기/기본수학1/No_1193.java
@@ -0,0 +1,26 @@
+package BOJ.단계별로풀어보기.기본수학1;
+import java.util.Scanner;
+public class No_1193 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    int X = in.nextInt();
+
+    int count = 0; // 분수 개수
+    int line = 0; // 행 번호
+
+    while (true) {
+      line++;
+      count += line;
+      if (count >= X) {
+        if (line % 2 == 0) {
+          System.out.println(X - count + line + "/" + (count - X + 1));
+        }
+        else {
+          System.out.println((count - X + 1) + "/" + (X - count +line));
+        }
+        break;
+      }
+    }
+  }
+}
