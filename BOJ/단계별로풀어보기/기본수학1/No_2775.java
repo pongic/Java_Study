f36f0638e7d5a240050fddcb033db09643b3bd54
pongic <pongic@naver.com>
2022-10-07 22:06:12 +0900

    2022.10.07 update

---
+++ BOJ/단계별로풀어보기/기본수학1/No_2775.java
@@ -0,0 +1,27 @@
+package BOJ.단계별로풀어보기.기본수학1;
+import java.io.*;
+
+public class No_2775 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    int T = Integer.parseInt(br.readLine());
+    int len = 15;
+    int[][] arr = new int[len][len]; // 2차원 배열 집
+
+    // 0층의 i호에는 i명이 산다
+    for (int i = 1; i < len;i++) {
+      arr[0][i] = i;
+    }
+
+
+
+    for (int i = 0; i < T; i++) {
+      int k = Integer.parseInt(br.readLine());
+      int n = Integer.parseInt(br.readLine());
+      sb.append(arr[k][n] + "\n");
+    }
+    System.out.println(sb);
+  }
+}
