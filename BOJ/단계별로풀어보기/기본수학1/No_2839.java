1ea1264e126fabca9245457f10af519373e6b195
pongic <pongic@naver.com>
2022-10-09 19:55:17 +0900

    2022.10.09 update

--- BOJ/단계별로풀어보기/기본수학1/No_2839.java
+++ BOJ/단계별로풀어보기/기본수학1/No_2839.java
@@ -4,19 +4,27 @@ import java.io.*;
 public class No_2839 {
   public static void main(String[] args) throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int[] bag = {5, 3};
     int count = 0;
-    int result = 0;
 
     int N = Integer.parseInt(br.readLine());
 
-    for (int i = 0; i < bag.length; i++) {
-      if (N > 0) {
-        count = N / bag[i];
-        result += count;
-        N = N - (bag[i] * count);
+    while (true) {
+      // N이 음수일 경우 -1출력 하면서 종료
+      if (N < 0) {
+        count = -1;
+        break;
+      }
+      // N이 5로 나누어지는 경우
+      if (N % 5 == 0) {
+        count += N / 5;
+        break;
+      }
+      // N이 5로 나누어지지 않는 경우 N을 3씩 빼고 COUNT 증가
+      else {
+        N -= 3;
+        count++;
       }
     }
-    System.out.println(result);
+    System.out.println(count);
   }
 }
