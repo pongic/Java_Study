e280ce280231796e6b88b937196c403fed51ffab
pongic <pongic@naver.com>
2022-10-12 23:14:14 +0900

    골드바흐의 추측

--- BOJ/단계별로풀어보기/기본수학2/No_9020.java
+++ BOJ/단계별로풀어보기/기본수학2/No_9020.java
@@ -1,12 +1,34 @@
 package BOJ.단계별로풀어보기.기본수학2;
 import java.io.*;
 public class No_9020 {
-  public static boolean[] check;
+  public static boolean[] check = new boolean[10001];
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+
+    numberCheck();
 
     int T = Integer.parseInt(br.readLine());
 
+    while (T > 0) {
+      int N = Integer.parseInt(br.readLine());
+      int fisrt = N / 2;
+      int second = N / 2;
+      T--;
+      while (true) {
+        // first 와 second 둘 다 소수일 경우
+        if (!check[fisrt] && !check[second]) {
+          sb.append(second).append(' ').append(fisrt).append('\n');
+          break;
+        }
+        fisrt++;
+        second--;
+      }
+    }
+    System.out.println(sb);
+
+
+    /*
     // 수 입력
     for (int i = 0; i < T; i++) {
       int N = Integer.parseInt(br.readLine());
@@ -26,6 +48,7 @@ public class No_9020 {
       }
       System.out.println(index + " " + (N - index));
     }
+     */
   }
   // 소수 구하는 메서드
   public static void numberCheck() {
