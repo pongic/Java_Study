61239e87d74daa50c5c8a29d3cd53f5e29ca0318
pongic <pongic@naver.com>
2022-10-18 20:16:09 +0900

    통계학

--- BOJ/단계별로풀어보기/정렬/No_2108.java
+++ BOJ/단계별로풀어보기/정렬/No_2108.java
@@ -15,9 +15,34 @@ public class No_2108 {
     }
     Arrays.sort(numbers);
 
+    // 최빈값
+    int count = 0;
+    int max = -1;
+    int tmp = numbers[0];
+    boolean check = false;
+
+    for (int i = 0; i < N - 1; i++) {
+      if (numbers[i] == numbers[i + 1]) {
+        count++;
+      }
+      else {
+        count = 0;
+      }
+
+      if (max < count) {
+        max = count;
+        tmp = numbers[i];
+        check = true;
+      }
+      else if (max == count && check == true) {
+        tmp = numbers[i];
+        check = false;
+      }
+    }
+
     System.out.println(Math.round(sum / N));
     System.out.println(numbers[(N - 1) / 2]);
-    //System.out.println(result3);
+    System.out.println(tmp);
     System.out.println(numbers[numbers.length - 1] - numbers[0]);
   }
 }
