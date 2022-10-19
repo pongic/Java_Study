51e643c446d9fa6fa5cd11b31347aaf3d4aa666b
pongic <pongic@naver.com>
2022-10-19 23:03:46 +0900

    단어 정렬

---
+++ BOJ/단계별로풀어보기/정렬/No_1181.java
@@ -0,0 +1,24 @@
+package BOJ.단계별로풀어보기.정렬;
+import java.util.Arrays;
+import java.util.Scanner;
+public class No_1181 {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    int N = in.nextInt();
+    String[] arr = new String[N];
+    int[] len = new int[N];
+    String[] result = new String[N];
+
+    for (int i = 0; i < N; i++) {
+      arr[i] = in.nextLine();
+      len[i] = arr[i].length();
+    }
+    Arrays.sort(len);
+
+//    for (int i = 0; i < N; i++) {
+//
+//    }
+
+  }
+}
