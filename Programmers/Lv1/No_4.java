b4ae18e34cbc9e0c4523e56688dc119b2016d354
pongic <pongic@naver.com>
2022-10-23 21:53:50 +0900

    자릿수 더하기

--- Programmers/Lv1/No_4.java
+++ Programmers/Lv1/No_4.java
@@ -1,8 +1,13 @@
 package Programmers.Lv1;
 
 public class No_4 {
-  public static void main(String[] args) {
-    int i = 123;
-    System.out.println(i %10);
+  public int solution(int n) {
+    int answer = 0;
+
+    while (n > 0) {
+      answer += n % 10;
+      n = n / 10;
+    }
+    return answer;
   }
 }
