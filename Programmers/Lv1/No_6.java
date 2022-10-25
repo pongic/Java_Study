ed92a997f4c286041389f8ee223c310c82f34b5c
pongic <pongic@naver.com>
2022-10-25 18:48:13 +0900

    자연수 뒤집어 배열로 만들기

---
+++ Programmers/Lv1/No_6.java
@@ -0,0 +1,19 @@
+package Programmers.Lv1;
+
+public class No_6 {
+  class Solution {
+    public int[] solution(long n) {
+      String str = String.valueOf(n);
+      StringBuilder sb = new StringBuilder(str);
+      sb = sb.reverse();
+
+      String[] arr = sb.toString().split("");
+      int[] answer = new int[sb.length()];
+
+      for (int i = 0; i < sb.length(); i++) {
+        answer[i] = Integer.parseInt(arr[i]);
+      }
+      return answer;
+    }
+  }
+}
