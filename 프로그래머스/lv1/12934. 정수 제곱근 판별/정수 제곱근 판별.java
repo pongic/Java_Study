e1d40e2e53f8937e2c6ad8659b1e1bd512df8fe2
pongic <73628801+pongic@users.noreply.github.com>
2022-10-29 13:35:59 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        Double num = Math.sqrt(n);
+        if (num == num.intValue()) {
+            answer = (long)Math.pow(num + 1, 2);
+        }
+        else {
+            answer = -1;
+        }
+        return answer;
+    }
+}
