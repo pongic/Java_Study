5e83a36c617a4cee524bc919e18a3e3bf9e33db0
pongic <73628801+pongic@users.noreply.github.com>
2022-11-08 21:33:50 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.04 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int n, int m) {
+        int max = Math.max(n,m);
+        int min = Math.min(n,m);
+        
+        while (min != 0) {
+            int r = max % min;
+            max = min;
+            min = r;
+        }
+        
+        return new int[] {max, n * m / max};
+    }
+}
